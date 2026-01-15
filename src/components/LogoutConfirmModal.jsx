b1fc48dcec47f85b971b7647c007bee782d190e7
Nethmi Anjani <anjaninethmi23@gmail.com>
2026-01-15 10:53:48 +0530

    style updated

--- src/components/LogoutConfirmModal.jsx
+++ src/components/LogoutConfirmModal.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LogoutConfirmModal = ({isOpen, onCancel, onConfirm}) => {
     if (!isOpen) return null
   return (
-      <div className='fixed inset-0 z-[100] flex justify-center items-center bg-[#00170cb7]' >
+      <div className='fixed inset-0 z-[150] flex justify-center items-center bg-[#00170cb7]' >
           <div className="bg-white p-6 rounded-xl shadow-lg text-center">
               <h2 className="text-lg font-semibold mb-4">
                   Are you sure you want to logout?
